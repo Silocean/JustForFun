b19bbe6d556fe47ef965c7cae668e5658297375b
Tracy Roy <silenceocean@live.com>
2017-08-18 12:23:31 +0800

    Update Test全排列.java

--- LanqiaoOJ/2014lanqiaoOJ/src/Test全排列.java
+++ LanqiaoOJ/2014lanqiaoOJ/src/Test全排列.java
@@ -3,26 +3,26 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public class TestÈ«ÅÅÁĞ {
+public class TestÃˆÂ«Ã…Ã…ÃÃ {
 	
 	public static int n = 0;
 	
 	public static void main(String[] args) {
 		List<String> list = new LinkedList<String>(Arrays.asList("1","2","3","4","5"));
-        listAll(list, "");
-        System.out.println(n);
+        	listAll(list, "");
+        	System.out.println(n);
 	}
 	
 	public static void listAll(List<String> list, String strNum) {
-        if (list.isEmpty()) {
-            n++;
-            System.out.println(strNum);
-        }
-        for (int i = 0; i < list.size(); i++) {
-            List<String> temp = new LinkedList<String>(list);
-            String c = temp.remove(i);
-            listAll(temp, strNum + c);
-        }
-    }
+        	if (list.isEmpty()) {
+		    n++;
+		    System.out.println(strNum);
+        	}
+		for (int i = 0; i < list.size(); i++) {
+		    List<String> temp = new LinkedList<String>(list);
+		    String c = temp.remove(i);
+		    listAll(temp, strNum + c);
+		}
+    	}
 
 }
